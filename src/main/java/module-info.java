0af2427704ce4f39b9789816cce3521e280294ad
bryanvullo <bryanvullo04@gmail.com>
2024-03-04 23:43:53 +0000

    got fxml to work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,7 @@ module uk.ac.soton.comp2211 {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
+    
+    opens uk.ac.soton.comp2211 to javafx.fxml;
     exports uk.ac.soton.comp2211;
 }
