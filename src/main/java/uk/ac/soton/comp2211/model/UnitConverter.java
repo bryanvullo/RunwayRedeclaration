49d645d567473f8083f1247b4d5a9c5dd44cc4d8
deng <shunzed@outlook.com>
2024-03-06 14:22:24 +0000

    Add new feature

--- src/main/java/uk/ac/soton/comp2211/model/UnitConverter.java
+++ src/main/java/uk/ac/soton/comp2211/model/UnitConverter.java
@@ -8,4 +8,27 @@ public class UnitConverter {
     public static double feetToMeters(double feet) {
         return feet / 3.28084;
     }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
+    }
+    public static double metersToKilometers(double meters) {
+        return meters / 1000;
+    }
+
+    public static double kilometersToMeters(double kilometers) {
+        return kilometers * 1000;
+    }
+
+    public static double feetToMiles(double feet) {
+        return feet / 5280;
+    }
+
+    public static double milesToFeet(double miles) {
+        return miles * 5280;
+    }
 }
