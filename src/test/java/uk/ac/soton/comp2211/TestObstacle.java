e752176ff17ed52d80da4e3dcaf81e0a17cc36a7
Mo-E24 <mohannadelsaadawy@gmail.com>
2024-03-06 22:37:29 +0000

    amended test file to test for unrealistic values for dimensions of obstacles

--- src/test/java/uk/ac/soton/comp2211/TestObstacle.java
+++ src/test/java/uk/ac/soton/comp2211/TestObstacle.java
@@ -32,9 +32,24 @@ public class TestObstacle {
     @Test
     void testUnrealisticDimensionsRejected() {
         assertThrows(IllegalArgumentException.class, () -> {
-            // Attempt to create an AdvancedObstacle with unrealistic width and length values
+            // Attempt to create an AdvancedObstacle with unrealistic height, width and length values
             new AdvancedObstacle("Unrealistically Large Obstacle", 200000, 100001, 100002, 50, 50, 50, Optional.empty());
-        }, "Obstacle dimensions should be within realistic limits.");
+            }, "Obstacle dimensions should be within realistic limits.");
+
+            // Test for height
+            assertThrows(IllegalArgumentException.class, () -> {
+                new AdvancedObstacle("Unrealistically Large Obstacle", 200001, 50, 50, 100, 0, 0, Optional.empty());
+            }, "Height is unrealistically large and should not be accepted.");
+
+            // Test for width
+            assertThrows(IllegalArgumentException.class, () -> {
+                new AdvancedObstacle("Unrealistically Large Obstacle", 20, 100001, 50, 20, 50, 0, Optional.empty());
+            }, "Width is unrealistically large and should not be accepted.");
+
+            // Test for length
+            assertThrows(IllegalArgumentException.class, () -> {
+                new AdvancedObstacle("Unrealistically Large Obstacle", 20, 50, 100002, 500, 0, 110, Optional.empty());
+            }, "Length is unrealistically large and should not be accepted.");
     }
 
     // This tests for proper functioning of specialised methods like estimateClearanceTime for Plane
