0fd28c2e6163fbe586bde678057d8a936ff624d8
bryanvullo <bryanvullo04@gmail.com>
2024-03-07 00:31:41 +0000

    can now do very restricted calculation in the tool using only the 09L sample calculation runway

---
+++ src/main/java/uk/ac/soton/comp2211/model/Tool.java
@@ -0,0 +1,42 @@
+package uk.ac.soton.comp2211.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Tool {
+    private Calculation revisedCalculation;
+    
+    private Calculation previousCalculation;
+    
+    private Runway runway;
+    
+    private Runway secondarRunway;
+    
+    public IntegerProperty tora;
+    public IntegerProperty toda;
+    public IntegerProperty asda;
+    public IntegerProperty lda;
+    
+    public Tool() {
+        //TODO implement function to be able to change the runway
+        runway = new Runway("Heathrow 09L", 3902, 3902, 3902, 3595);
+        runway.setDisplacedThreshold(306);
+        
+        tora = new SimpleIntegerProperty(runway.getTora());
+        toda = new SimpleIntegerProperty(runway.getToda());
+        asda = new SimpleIntegerProperty(runway.getAsda());
+        lda = new SimpleIntegerProperty(runway.getLda());
+    }
+    
+    public void recalculate(Obstacle obstacle) {
+        previousCalculation = revisedCalculation;
+        
+        revisedCalculation = new Calculation(runway, obstacle);
+        revisedCalculation.calculateTakeOffAwayLandingOver();
+        
+        tora.set(revisedCalculation.getTora());
+        toda.set(revisedCalculation.getToda());
+        asda.set(revisedCalculation.getAsda());
+        lda.set(revisedCalculation.getLda());
+    }
+}
