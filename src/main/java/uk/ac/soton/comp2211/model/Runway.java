0a3fcc5e87e0fc26a712b4bbec0ce80e6358619e
bryanvullo <bryanvullo04@gmail.com>
2024-03-12 20:57:04 +0000

    obstacles in a new package - changed import

--- src/main/java/uk/ac/soton/comp2211/model/Runway.java
+++ src/main/java/uk/ac/soton/comp2211/model/Runway.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import uk.ac.soton.comp2211.model.obstacles.Obstacle;
 
 /**
  * This class to represents and models a Runway.
