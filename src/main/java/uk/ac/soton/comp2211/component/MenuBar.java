44190c1fd483088d410a3579e7dab77853845b94
bryanvullo <bryanvullo04@gmail.com>
2024-03-13 00:12:38 +0000

    some spacing adjustments and using user image as button graphic

--- src/main/java/uk/ac/soton/comp2211/component/MenuBar.java
+++ src/main/java/uk/ac/soton/comp2211/component/MenuBar.java
@@ -1,9 +1,13 @@
 package uk.ac.soton.comp2211.component;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.ToolBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,22 +22,31 @@ public class MenuBar extends HBox {
     
     public void build() {
         logger.info("Building the MenuBar");
+        setSpacing(10);
+        setPadding(new Insets(10));
+        setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
         
-        var toolbar = new ToolBar();
+//        var toolbar = new ToolBar();
+//        toolbar.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
         var fileButton = new MenuButton("File");
         var editButton = new MenuButton("Edit");
         var viewButton = new MenuButton("View");
         var settingsButton = new MenuButton("Settings");
         var helpButton = new MenuButton("Help");
         
-        toolbar.getItems().addAll(fileButton, editButton, viewButton, settingsButton, helpButton);
-        getChildren().add(toolbar);
+//        toolbar.getItems().addAll(fileButton, editButton, viewButton, settingsButton, helpButton);
+//        getChildren().add(toolbar);
+        getChildren().addAll(fileButton, editButton, viewButton, settingsButton, helpButton);
         
         var filler = new HBox();
         getChildren().add(filler);
         setHgrow(filler, Priority.ALWAYS);
         
-        var userButton = new MenuButton("User");
+        var userButton = new MenuButton();
+        var userImage = new ImageView(getClass().getResource("/img/user.png").toExternalForm());
+        userImage.setFitWidth(20);
+        userImage.setFitHeight(20);
+        userButton.setGraphic(userImage);
         getChildren().add(userButton);
     }
     
