45a593dbc1c8042bbdc7d5d90a6c452b10e64ff9
bryanvullo <bryanvullo04@gmail.com>
2024-03-13 16:07:56 +0000

    added previous and revised integer property fields

--- src/main/java/uk/ac/soton/comp2211/model/Tool.java
+++ src/main/java/uk/ac/soton/comp2211/model/Tool.java
@@ -13,28 +13,55 @@ public class Tool {
     
     private Runway secondarRunway;
     
+    //original values
     public IntegerProperty tora;
     public IntegerProperty toda;
     public IntegerProperty asda;
     public IntegerProperty lda;
+    
+    //revised values
+    public IntegerProperty revisedTora;
+    public IntegerProperty revisedToda;
+    public IntegerProperty revisedAsda;
+    public IntegerProperty revisedlLda;
+    
+    //previous values
+    public IntegerProperty previousTora;
+    public IntegerProperty previousToda;
+    public IntegerProperty previousAsda;
+    public IntegerProperty previousLda;
     
     public Tool() {
         tora = new SimpleIntegerProperty(0);
         toda = new SimpleIntegerProperty(0);
         asda = new SimpleIntegerProperty(0);
         lda = new SimpleIntegerProperty(0);
+        
+        revisedTora = new SimpleIntegerProperty(0);
+        revisedToda = new SimpleIntegerProperty(0);
+        revisedAsda = new SimpleIntegerProperty(0);
+        revisedlLda = new SimpleIntegerProperty(0);
+        
+        previousTora = new SimpleIntegerProperty(0);
+        previousToda = new SimpleIntegerProperty(0);
+        previousAsda = new SimpleIntegerProperty(0);
+        previousLda = new SimpleIntegerProperty(0);
     }
     
     public void recalculate(Obstacle obstacle) {
         previousCalculation = revisedCalculation;
+        previousTora.set(revisedTora.get());
+        previousToda.set(revisedToda.get());
+        previousAsda.set(revisedAsda.get());
+        previousLda.set(revisedlLda.get());
         
         revisedCalculation = new Calculation(runway, obstacle);
         revisedCalculation.calculateTakeOffAwayLandingOver();
         
-        tora.set(revisedCalculation.getTora());
-        toda.set(revisedCalculation.getToda());
-        asda.set(revisedCalculation.getAsda());
-        lda.set(revisedCalculation.getLda());
+        revisedTora.set(revisedCalculation.getTora());
+        revisedToda.set(revisedCalculation.getToda());
+        revisedAsda.set(revisedCalculation.getAsda());
+        revisedlLda.set(revisedCalculation.getLda());
     }
     
     public void setRunway(Runway runway) {
@@ -44,5 +71,19 @@ public class Tool {
         toda.set(runway.getToda());
         asda.set(runway.getAsda());
         lda.set(runway.getLda());
+        
+        revisedTora.set(0);
+        revisedToda.set(0);
+        revisedAsda.set(0);
+        revisedlLda.set(0);
+        
+        previousTora.set(0);
+        previousToda.set(0);
+        previousAsda.set(0);
+        previousLda.set(0);
+    }
+    
+    public Runway getRunway() {
+        return runway;
     }
 }
