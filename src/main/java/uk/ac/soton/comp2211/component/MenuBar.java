8895c7e9393b8c61aa419ddc09949da8db69c1a1
bryanvullo <bryanvullo04@gmail.com>
2024-03-13 13:45:46 +0000

    exposed the button

--- src/main/java/uk/ac/soton/comp2211/component/MenuBar.java
+++ src/main/java/uk/ac/soton/comp2211/component/MenuBar.java
@@ -2,6 +2,7 @@ package uk.ac.soton.comp2211.component;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -14,6 +15,13 @@ import org.apache.logging.log4j.Logger;
 public class MenuBar extends HBox {
     
     private static final Logger logger = LogManager.getLogger(MenuBar.class);
+    
+    private MenuButton fileButton;
+    private MenuButton editButton;
+    private MenuButton viewButton;
+    private MenuButton settingsButton;
+    private MenuButton helpButton;
+    private MenuButton userButton;
     
     public MenuBar() {
         logger.info("Creating the MenuBar");
@@ -28,11 +36,11 @@ public class MenuBar extends HBox {
         
 //        var toolbar = new ToolBar();
 //        toolbar.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
-        var fileButton = new MenuButton("File");
-        var editButton = new MenuButton("Edit");
-        var viewButton = new MenuButton("View");
-        var settingsButton = new MenuButton("Settings");
-        var helpButton = new MenuButton("Help");
+        fileButton = new MenuButton("File");
+        editButton = new MenuButton("Edit");
+        viewButton = new MenuButton("View");
+        settingsButton = new MenuButton("Settings");
+        helpButton = new MenuButton("Help");
         
 //        toolbar.getItems().addAll(fileButton, editButton, viewButton, settingsButton, helpButton);
 //        getChildren().add(toolbar);
@@ -42,12 +50,42 @@ public class MenuBar extends HBox {
         getChildren().add(filler);
         setHgrow(filler, Priority.ALWAYS);
         
-        var userButton = new MenuButton();
+        userButton = new MenuButton();
         var userImage = new ImageView(getClass().getResource("/img/user.png").toExternalForm());
         userImage.setFitWidth(20);
         userImage.setFitHeight(20);
         userButton.setGraphic(userImage);
+        userButton.getItems().addAll(
+            new MenuItem("Username"),
+            new MenuItem("Access Level"),
+            new MenuItem("Logout"));
+        
         getChildren().add(userButton);
     }
+    
+    public MenuButton fileButton() {
+        return fileButton;
+    }
+    
+    public MenuButton editButton() {
+        return editButton;
+    }
+    
+    public MenuButton viewButton() {
+        return viewButton;
+    }
+    
+    public MenuButton settingsButton() {
+        return settingsButton;
+    }
+    
+    public MenuButton helpButton() {
+        return helpButton;
+    }
+    
+    public MenuButton userButton() {
+        return userButton;
+    }
+    
     
 }
