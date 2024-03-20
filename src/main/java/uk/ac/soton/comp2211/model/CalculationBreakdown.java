7b5e17c7a60a3dd0acd1ff67dc0a1362584205a4
bryanvullo <bryanvullo04@gmail.com>
2024-03-20 22:44:31 +0000

    refactored the Strings to StringProperties and exposed these for binding

--- src/main/java/uk/ac/soton/comp2211/model/CalculationBreakdown.java
+++ src/main/java/uk/ac/soton/comp2211/model/CalculationBreakdown.java
@@ -1,11 +1,14 @@
 package uk.ac.soton.comp2211.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class CalculationBreakdown {
     
-    private String toraBreakdown;
-    private String todaBreakdown;
-    private String asdaBreakdown;
-    private String ldaBreakdown;
+    private StringProperty toraBreakdown;
+    private StringProperty todaBreakdown;
+    private StringProperty asdaBreakdown;
+    private StringProperty ldaBreakdown;
     private String tocsBreakdown;
     private String alsBreakdown;
     private String blastProctectionBreakdown;
@@ -15,6 +18,11 @@ public class CalculationBreakdown {
         var obstacle = calculation.getObstacle();
         var runway = calculation.getRunway();
         
+        toraBreakdown = new SimpleStringProperty();
+        todaBreakdown = new SimpleStringProperty();
+        asdaBreakdown = new SimpleStringProperty();
+        ldaBreakdown = new SimpleStringProperty();
+        
         switch (calculationType) {
             case "TOALO": // Take-Off Away and Landing Over
                 // Calculate the breakdown of the TORA, TODA, ASDA, LDA, ALS, and Blast Protection
@@ -22,34 +30,34 @@ public class CalculationBreakdown {
                     + " = 240 + 60 \n"
                     + " = 300";
                 if (runway.getDisplacedThreshold() != null) {
-                    toraBreakdown = " = Original TORA - Distance from the threshold - Displaced Threshold - Blast Protection \n"
+                    toraBreakdown.set(" = Original TORA - Distance from the threshold - Displaced Threshold - Blast Protection \n"
                         + String.format(" = %f - %f - %f - 300 \n", runway.getTora(), obstacle.getDistanceFromThreshold(), runway.getDisplacedThreshold())
-                        + " = " + calculation.getTora();
+                        + " = " + calculation.getTora());
                 } else {
-                    toraBreakdown = " = Original TORA - Distance from the threshold - Blast Protection \n"
+                    toraBreakdown.set(" = Original TORA - Distance from the threshold - Blast Protection \n"
                         + String.format(" = %f - %f - 300 \n", runway.getTora(), obstacle.getDistanceFromThreshold())
-                        + " = " + calculation.getTora();
+                        + " = " + calculation.getTora());
                 }
                 if (runway.getClearway() != null) {
-                    todaBreakdown = " = TORA + Clearway \n"
+                    todaBreakdown.set(" = TORA + Clearway \n"
                         + String.format(" = %f + %f \n", calculation.getTora(), runway.getClearway())
-                        + " = " + calculation.getToda();
+                        + " = " + calculation.getToda());
                 } else {
-                    todaBreakdown = " = TORA \n = " + calculation.getTora();
+                    todaBreakdown.set(" = TORA \n = " + calculation.getTora());
                 }
                 if (runway.getStopway() != null) {
-                    asdaBreakdown = " = TORA + Stopway \n"
+                    asdaBreakdown.set(" = TORA + Stopway \n"
                         + String.format(" = %f + %f \n", calculation.getTora(), runway.getStopway())
-                        + " = " + calculation.getAsda();
+                        + " = " + calculation.getAsda());
                 } else {
-                    asdaBreakdown = " = TORA \n = " + calculation.getTora();
+                    asdaBreakdown.set(" = TORA \n = " + calculation.getTora());
                 }
                 alsBreakdown = String.format(" = max(RESA, 50 x %f) \n", obstacle.getHeight())
                     + String.format(" = max(240, %f) \n", 50 * obstacle.getHeight())
                     + String.format(" = %f", Math.max(240, 50 * obstacle.getHeight()));
-                ldaBreakdown = " = Original LDA - Distance from the threshold - Strip End - ALS \n"
+                ldaBreakdown.set(" = Original LDA - Distance from the threshold - Strip End - ALS \n"
                     + String.format(" = %f - %f - 60 - %f \n", runway.getLda(), obstacle.getDistanceFromThreshold(), Math.max(240, 50 * obstacle.getHeight()))
-                    + " = " + calculation.getLda();
+                    + " = " + calculation.getLda() + "\n(ALS = " + alsBreakdown + ")");
                 break;
             case "TOTLT": // Take-Off Towards and Landing Towards
                 // Calculate the breakdown of the TORA, TODA, ASDA, LDA, and TOCS
@@ -58,24 +66,39 @@ public class CalculationBreakdown {
                     + String.format(" = %f", Math.max(240, 50 * obstacle.getHeight()));
                 
                 if (runway.getDisplacedThreshold() != null) {
-                    toraBreakdown = " = Distance from the threshold + Displaced Threshold - TOCS - Strip End \n"
+                    toraBreakdown.set(" = Distance from the threshold + Displaced Threshold - TOCS - Strip End \n"
                         + String.format(" = %f + %f - %f - 60 \n", obstacle.getDistanceFromThreshold(), runway.getDisplacedThreshold(), Math.max(240, 50 * obstacle.getHeight()))
-                        + " = " + calculation.getTora();
+                        + " = " + calculation.getTora());
                 } else {
-                    toraBreakdown = " = Distance from the threshold - TOCS - Strip End \n"
+                    toraBreakdown.set(" = Distance from the threshold - TOCS - Strip End \n"
                         + String.format(" = %f - %f - 60 \n", obstacle.getDistanceFromThreshold(), Math.max(240, 50 * obstacle.getHeight()))
-                        + " = " + calculation.getTora();
+                        + " = " + calculation.getTora());
                 }
-                todaBreakdown = " = TORA \n = " + calculation.getTora();
-                asdaBreakdown = " = TORA \n = " + calculation.getTora();
-                ldaBreakdown = " = Distance from the threshold - RESA - Strip End \n"
+                todaBreakdown.set(" = TORA \n = " + calculation.getTora());
+                asdaBreakdown.set(" = TORA \n = " + calculation.getTora());
+                ldaBreakdown.set(" = Distance from the threshold - RESA - Strip End \n"
                     + String.format(" = %f - 240 - 60 \n", obstacle.getDistanceFromThreshold())
-                    + " = " + calculation.getLda();
+                    + " = " + calculation.getLda() + "\n(TOCS = " + tocsBreakdown + ")");
                 break;
         }
         
         
     }
     
-
+    
+    public StringProperty getToraBreakdown() {
+        return toraBreakdown;
+    }
+    
+    public StringProperty getTodaBreakdown() {
+        return todaBreakdown;
+    }
+    
+    public StringProperty getAsdaBreakdown() {
+        return asdaBreakdown;
+    }
+    
+    public StringProperty getLdaBreakdown() {
+        return ldaBreakdown;
+    }
 }
