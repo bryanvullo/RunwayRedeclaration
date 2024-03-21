bff73d5549ead1ffbf5721dfcf46c47303581440
DwijeshD <dwijeshdookraz1@gmail.com>
2024-03-21 00:16:23 +0000

    Minor Changes

--- src/main/java/uk/ac/soton/comp2211/scene/MainScene.java
+++ src/main/java/uk/ac/soton/comp2211/scene/MainScene.java
@@ -1,5 +1,7 @@
 package uk.ac.soton.comp2211.scene;
 
+import java.io.File;
+import java.io.IOException;
 import javafx.event.Event;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -19,8 +21,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPath;
+import java.util.logging.Logger;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import uk.ac.soton.comp2211.UI.AppWindow;
 import uk.ac.soton.comp2211.UI.AppPane;
 import uk.ac.soton.comp2211.component.ActiveObstacle;
@@ -44,14 +55,14 @@ import uk.ac.soton.comp2211.model.obstacles.Plane;
 import uk.ac.soton.comp2211.model.obstacles.ShuttleBus;
 
 public class MainScene extends BaseScene {
-    
-    private static final Logger logger = LogManager.getLogger(MainScene.class);
-    
+
+    private static final Logger logger = Logger.getLogger(MainScene.class.getName());
+
     //Backend Fields
     private Tool tool;
     private Runway selectedRunway = null;
-    private Obstacle selectedObstacle = null;
-    
+    private AdvancedObstacle selectedObstacle = null;
+
     //Collapse panel variables
     private Boolean leftPanelCollapsed = false;
     private Boolean rightPanelCollapsed = false;
@@ -64,31 +75,35 @@ public class MainScene extends BaseScene {
     private VBox leftBar;
     private VBox activeBar;
     private ActiveObstacle activeObstacle;
+    private RunwayBox runwayBox;
+
+    private String selectedDirection = null;
     private RunwayViewBox runwayViewBox;
-    
-    
+
+
     public MainScene(AppWindow appWindow) {
         super(appWindow);
     }
-    
+
     @Override
     public void initialise() {
         logger.info("initialising the menu scene");
+        //this is for all backend stuff
     }
-    
+
     @Override
     public void build() {
         logger.info("Building " + this.getClass().getName());
-        
+
         tool = new Tool();
-        
+
         root = new AppPane(appWindow.getWidth(),appWindow.getHeight());
-        
+
         var mainPane = new BorderPane();
         mainPane.setMaxWidth(appWindow.getWidth());
         mainPane.setMaxHeight(appWindow.getHeight());
         root.getChildren().add(mainPane);
-        
+
         runwayViewBox = new RunwayViewBox();
         Button sideButton = runwayViewBox.getSideButton();
         Button simultaneousButton = runwayViewBox.getSimultaneousButton();
@@ -98,7 +113,7 @@ public class MainScene extends BaseScene {
         simultaneousButton.setOnAction(event -> runwayViewBox.changeViewToSimultaneous());
         //this is for all backend stuff
         mainPane.setCenter(runwayViewBox);
-        
+
         //Todo: tool bar MenuItems and functionality
         //Toolbar at the top
         var toolbar = new MenuBar();
@@ -113,10 +128,10 @@ public class MainScene extends BaseScene {
         leftCollapseButton = new Button("<");
         leftCollapsibleBar = new VBox(leftCollapseButton);
         leftCollapsibleBar.setAlignment(Pos.CENTER);
-        
+
         leftBar = new VBox();
         leftBar.setAlignment(Pos.TOP_CENTER);
-        var runwayBox = new RunwayBox();
+        runwayBox = new RunwayBox();
         var obstacleBox = new ObstaclesBox();
         leftBar.getChildren().addAll(runwayBox,obstacleBox);
         VBox.setVgrow(runwayBox, Priority.ALWAYS);
@@ -133,7 +148,7 @@ public class MainScene extends BaseScene {
         rightCollapseButton = new Button(">");
         rightCollapsibleBar = new VBox(rightCollapseButton);
         rightCollapsibleBar.setAlignment(Pos.CENTER);
-        
+
         activeBar = new VBox();
         activeBar.setAlignment(Pos.TOP_CENTER);
         activeObstacle = new ActiveObstacle();
@@ -143,7 +158,7 @@ public class MainScene extends BaseScene {
         VBox.setVgrow(calculationBreakdownBox, Priority.ALWAYS);
         rightPanel.getChildren().addAll(rightCollapsibleBar, activeBar);
         mainPane.setRight(rightPanel);
-        
+
         //TODO system messages
         //Bottom bar: System message and calculation tab
         var bottomBar = new HBox();
@@ -154,11 +169,11 @@ public class MainScene extends BaseScene {
         HBox.setHgrow(systemMessageBox, Priority.ALWAYS);
         HBox.setHgrow(calcTab, Priority.ALWAYS);
         mainPane.setBottom(bottomBar);
-        
+
         ///////////////////////Configure the UI///////////////////////
         toolbar.userButton().getItems().get(0).setDisable(true);
         toolbar.userButton().getItems().get(1).setDisable(true);
-        
+
         //calculation tab binding
         calcTab.orignalTora.bind(tool.tora);
         calcTab.originalToda.bind(tool.toda);
@@ -172,10 +187,14 @@ public class MainScene extends BaseScene {
         calcTab.previousToda.bind(tool.previousToda);
         calcTab.previousAsda.bind(tool.previousAsda);
         calcTab.previousLda.bind(tool.previousLda);
-        
+
+        runwayBox.clearwayProperty().bind(tool.clearway);
+        runwayBox.stopwayProperty().bind(tool.stopway);
+        runwayBox.displacedThresholdProperty().bind(tool.displacedThreshold);
+
         leftCollapseButton.setOnAction(this::collapseLeftPanel);
         rightCollapseButton.setOnAction(this::collapseRightPanel);
-        
+
         //Adding back end functionality
         calcTab.recalculateButton().setOnAction((e) -> {
             logger.info("Recalculate button pressed");
@@ -192,58 +211,166 @@ public class MainScene extends BaseScene {
                 alert.setContentText("Please select a runway before calculating");
                 alert.showAndWait();
             } else  {
-                recalculate(selectedRunway, selectedObstacle);
+                recalculate();
             }
-            
+
         });
-        
+
         obstacleBox.getBoeingButton().setOnAction((e) -> {
             logger.info("Boeing Button Pressed");
-            
+
             var obstacle = Plane.createPlane("Boeing 747");
-            
+
             updateObstacleLocation(obstacle);
-            
+
             updateObstacle( obstacle );
         });
         obstacleBox.getAirbusButton().setOnAction((e) -> {
             logger.info("Airbus Button Pressed");
-            
+
             var obstacle = Plane.createPlane("Airbus A380");
-            
+
             updateObstacleLocation(obstacle);
-            
+
             updateObstacle( obstacle );
         });
         obstacleBox.getContainerButton().setOnAction((e) -> {
             logger.info("Container Button Pressed");
-            
+
             var obstacle = Container.createContainer();
-            
+
             updateObstacleLocation(obstacle);
-            
+
             updateObstacle( obstacle );
         });
         obstacleBox.getShuttleBusButton().setOnAction((e) -> {
             logger.info("Shuttle Bus Button Pressed");
-            
+
             var obstacle = ShuttleBus.createShuttleBus();
-            
+
             updateObstacleLocation(obstacle);
-            
+
             updateObstacle( obstacle );
         });
         obstacleBox.getCustomButton().setOnAction((e) -> {
             logger.info("Custom Button Pressed");
-            
+
             var obstacle = new AdvancedObstacle();
-            
+
             getInputAdvancedObstacle(obstacle);
-            
+
             updateObstacle( obstacle );
         });
+
+        runwayBox.getAirportSelection().setOnAction(this::selectAirport);
+        runwayBox.getRunwaySelection().setOnAction(this::selectRunway);
     }
-    
+
+    private void selectAirport(Event event) {
+        logger.info("Airport Selected");
+
+        var airport = (String) runwayBox.getAirportSelection().getSelectionModel().getSelectedItem();
+        runwayBox.getRunwaySelection().getItems().clear();
+        runwayBox.getRunwaySelection().setPromptText("Select Runway");
+
+        try {
+            //fetching the airport data
+            var builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            var doc = builder.parse(new File(getClass().getResource("/xml/airports.xml").getFile()));
+            var xpath = XPathFactory.newInstance().newXPath();
+            var search = "//airport/name[text()=\"" + airport + "\"]/parent::airport/runways/runway";
+            var nodes = (NodeList) xpath.compile(search).evaluate(doc, XPathConstants.NODESET);
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                var node = nodes.item(i);
+                var name = (String) xpath.compile("name/text()").evaluate(node, XPathConstants.STRING);
+                runwayBox.getRunwaySelection().getItems().add(name);
+            }
+            runwayBox.getRunwaySelection().setDisable(false);
+
+        } catch (ParserConfigurationException | SAXException | IOException |
+                 XPathExpressionException ex) {
+            //popup error message in case of failure
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Unable to fetch airport data");
+            alert.setContentText("Error: " + ex.getMessage());
+            alert.showAndWait();
+        }
+    }
+
+    private void selectRunway(Event e) {
+        logger.info("Runway Selected");
+
+        var airportName = (String) runwayBox.getAirportSelection().getSelectionModel().getSelectedItem();
+        var runwayName = (String) runwayBox.getRunwaySelection().getSelectionModel().getSelectedItem();
+        try {
+            var builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            var doc = builder.parse(new File(getClass().getResource("/xml/airports.xml").getFile()));
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            var search = "//airport/name[text()=\"" + airportName + "\"]/"
+                    + "parent::airport/runways/runway/name[text()=\"" + runwayName + "\"]"
+                    + "/parent::runway/logicalRunway";
+            var nodes = (NodeList) xpath.compile(search).evaluate(doc, XPathConstants.NODESET);
+
+            if (nodes.getLength() == 1) {
+                runwayBox.getLogicalLeftButton().setDisable(true);
+                runwayBox.getLogicalRightButton().setDisable(true);
+
+                var node = nodes.item(0);
+
+                var name = (String) xpath.compile("name").evaluate(node, XPathConstants.STRING);
+                var tora = (Double) xpath.compile("tora").evaluate(node, XPathConstants.NUMBER);
+                var toda = (Double) xpath.compile("toda").evaluate(node, XPathConstants.NUMBER);
+                var asda = (Double) xpath.compile("asda").evaluate(node, XPathConstants.NUMBER);
+                var lda = (Double) xpath.compile("lda").evaluate(node, XPathConstants.NUMBER);
+                var clearway = (Double) xpath.compile("clearway").evaluate(node, XPathConstants.NUMBER);
+                var stopway = (Double) xpath.compile("stopway").evaluate(node, XPathConstants.NUMBER);
+                var displacedThreshold = (Double) xpath.compile("displacedThreshold").evaluate(node, XPathConstants.NUMBER);
+
+                var runway = new Runway(name, tora, toda, asda, lda);
+                runway.setClearway(clearway);
+                runway.setStopway(stopway);
+                runway.setDisplacedThreshold(displacedThreshold);
+
+                updateRunway(runway);
+            } else if (nodes.getLength() == 2) {
+                runwayBox.getLogicalLeftButton().setDisable(true);
+                runwayBox.getLogicalRightButton().setDisable(false);
+                selectedDirection = "left";
+
+                search = search + "[@dir = \"left\"]";
+                var node = (Node) xpath.compile(search).evaluate(doc, XPathConstants.NODE);
+
+                var name = (String) xpath.compile("name").evaluate(node, XPathConstants.STRING);
+                var tora = (Double) xpath.compile("tora").evaluate(node, XPathConstants.NUMBER);
+                var toda = (Double) xpath.compile("toda").evaluate(node, XPathConstants.NUMBER);
+                var asda = (Double) xpath.compile("asda").evaluate(node, XPathConstants.NUMBER);
+                var lda = (Double) xpath.compile("lda").evaluate(node, XPathConstants.NUMBER);
+                var clearway = (Double) xpath.compile("clearway").evaluate(node, XPathConstants.NUMBER);
+                var stopway = (Double) xpath.compile("stopway").evaluate(node, XPathConstants.NUMBER);
+                var displacedThreshold = (Double) xpath.compile("displacedThreshold").evaluate(node, XPathConstants.NUMBER);
+
+                var runway = new Runway(name, tora, toda, asda, lda);
+                runway.setClearway(clearway);
+                runway.setStopway(stopway);
+                runway.setDisplacedThreshold(displacedThreshold);
+
+                updateRunway(runway);
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException |
+                 XPathExpressionException ex) {
+            //popup error message in case of failure
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Unable to fetch runway data");
+            alert.setContentText("Error: " + ex.getMessage());
+            alert.showAndWait();
+        }
+
+    }
+
     private void collapseLeftPanel(Event event) {
         if (leftPanelCollapsed) {
             leftCollapseButton.setText("<");
@@ -258,7 +385,7 @@ public class MainScene extends BaseScene {
             leftPanelCollapsed = true;
         }
     }
-    
+
     private void collapseRightPanel(Event event) {
         if (rightPanelCollapsed) {
             rightCollapseButton.setText(">");
@@ -273,27 +400,47 @@ public class MainScene extends BaseScene {
             rightPanelCollapsed = true;
         }
     }
-    
-    private void recalculate(Runway runway, Obstacle obstacle) {
-        tool.recalculate(obstacle);
+
+    private void recalculate() {
+        logger.info("Recalculating");
+
+        if (selectedDirection.equals("left")) {
+            selectedObstacle.setDistanceFromThreshold(selectedObstacle.getDistanceLeftThreshold());
+
+            if (selectedObstacle.getDistanceLeftThreshold() > selectedObstacle.getDistanceRightThreshold()) {
+                tool.recalculate(selectedObstacle, "TOTLT");
+            } else {
+                tool.recalculate(selectedObstacle, "TOALO");
+            }
+
+        } else if (selectedDirection.equals("right")) {
+            selectedObstacle.setDistanceFromThreshold(selectedObstacle.getDistanceRightThreshold());
+
+            if (selectedObstacle.getDistanceRightThreshold() > selectedObstacle.getDistanceLeftThreshold()) {
+                tool.recalculate(selectedObstacle, "TOTLT");
+            } else {
+                tool.recalculate(selectedObstacle, "TOALO");
+            }
+        }
+
     }
-    
+
     private void updateRunway(Runway runway) {
         selectedRunway = runway;
         tool.setRunway(runway);
     }
-    
+
     private void updateObstacleLocation(AdvancedObstacle obstacle) {
         var locationDialog = new ObstacleLocationDialog();
         locationDialog.setResultConverter(button -> {
             if (button == ButtonType.OK) {
                 return new ObstacleLocation(
-                    Double.parseDouble(locationDialog.getDistanceFromLeftThreshold()),
-                    Double.parseDouble(locationDialog.getDistanceFromRightThreshold()),
-                    Double.parseDouble(locationDialog.getDistanceFromCentre()));
+                        Double.parseDouble(locationDialog.getDistanceFromLeftThreshold()),
+                        Double.parseDouble(locationDialog.getDistanceFromRightThreshold()),
+                        Double.parseDouble(locationDialog.getDistanceFromCentre()));
             }
             return null;
-            });
+        });
         var optionalResult = locationDialog.showAndWait();
         optionalResult.ifPresent( (ObstacleLocation result) -> {
             obstacle.setDistanceLeftThreshold(result.getDistanceFromLeftThreshold());
@@ -301,23 +448,23 @@ public class MainScene extends BaseScene {
             obstacle.setDistanceFromCentre(result.getDistanceFromCentre());
         });
     }
-    
+
     private void getInputAdvancedObstacle(AdvancedObstacle customObstacle) {
-        
+
         var customObstacleDialog = new CustomObstacleDialog();
         customObstacleDialog.setResultConverter(button -> {
             if (button == ButtonType.OK) {
                 return new CustomObstacleLocation(
-                    customObstacleDialog.getName(),
-                    Double.parseDouble(customObstacleDialog.getHeightValue()),
-                    Double.parseDouble(customObstacleDialog.getWidthValue()),
-                    Double.parseDouble(customObstacleDialog.getLength()),
-                    Double.parseDouble(customObstacleDialog.getDistanceFromLeftThreshold()),
-                    Double.parseDouble(customObstacleDialog.getDistanceFromRightThreshold()),
-                    Double.parseDouble(customObstacleDialog.getDistanceFromCentre()));
+                        customObstacleDialog.getName(),
+                        Double.parseDouble(customObstacleDialog.getHeightValue()),
+                        Double.parseDouble(customObstacleDialog.getWidthValue()),
+                        Double.parseDouble(customObstacleDialog.getLength()),
+                        Double.parseDouble(customObstacleDialog.getDistanceFromLeftThreshold()),
+                        Double.parseDouble(customObstacleDialog.getDistanceFromRightThreshold()),
+                        Double.parseDouble(customObstacleDialog.getDistanceFromCentre()));
             }
             return null;
-            });
+        });
         var optionalResult = customObstacleDialog.showAndWait();
         optionalResult.ifPresent( (CustomObstacleLocation result) -> {
             customObstacle.setObstacleName(result.getName());
@@ -329,14 +476,13 @@ public class MainScene extends BaseScene {
             customObstacle.setDistanceFromCentre(result.getDistanceFromCentre());
         });
     }
-    
+
     private void updateObstacle(AdvancedObstacle obstacle) {
         selectedObstacle = obstacle;
         activeObstacle.update(obstacle);
     }
 
-  public Parent getRoot() {
-    return root;
-  }
+    public Parent getRoot() {
+        return root;
+    }
 }
-
