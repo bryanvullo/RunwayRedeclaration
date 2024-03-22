e2ca681c60b49e84cb8cc5e5e9798a4eff45bbff
bryanvullo <bryanvullo04@gmail.com>
2024-03-22 21:56:14 +0000

    deng's utility files

---
+++ src/main/java/uk/ac/soton/comp2211/Utility/NotificationService.java
@@ -0,0 +1,23 @@
+package uk.ac.soton.comp2211.Utility;
+
+
+import javafx.application.Platform;
+import uk.ac.soton.comp2211.control.TopDownViewController;
+import uk.ac.soton.comp2211.dataStructure.Notification;
+
+/**
+ * Handles creating and dispatching notifications to the UI.
+ */
+public class NotificationService {
+    private TopDownViewController controller;
+
+    public NotificationService(TopDownViewController controller) {
+        this.controller = controller;
+    }
+
+    public void notifyEvent(String date, String time, String message) {
+        Notification notification = new Notification(date, time, message);
+        // Dispatch notification to the UI thread
+        Platform.runLater(() -> controller.addNotification(notification));
+    }
+}
