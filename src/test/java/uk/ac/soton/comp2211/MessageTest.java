73a91f0da60c36cbfcd9cfa762591478586d90d1
SarveshMina <105467665+SarveshMina@users.noreply.github.com>
2024-03-23 04:15:56 +0000

    Commented Deng's code which doesn't work.

--- src/test/java/uk/ac/soton/comp2211/MessageTest.java
+++ src/test/java/uk/ac/soton/comp2211/MessageTest.java
@@ -1,36 +1,36 @@
-package uk.ac.soton.comp2211;
-
-import javafx.application.Platform;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import uk.ac.soton.comp2211.Utility.ErrorNotificationService;
-import uk.ac.soton.comp2211.control.TopDownViewController;
-import uk.ac.soton.comp2211.dataStructure.ErrorNotification;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-
-public class MessageTest {
-    private ErrorNotificationService service;
-    private TopDownViewController controller;
-
-    @BeforeEach
-    void setUp() {
-        controller = Mockito.mock(TopDownViewController.class);
-        service = new ErrorNotificationService(controller);
-    }
-
-    @BeforeAll
-    public static void setUpClass() {
-        Platform.startup(() -> {});
-    }
-
-    @Test
-    void testNotifyError() throws InterruptedException {
-        service.notifyError("2024-10-10 12:00", "Test Error Message");
-        Thread.sleep(100);
-        verify(controller).addErrorNotification(any(ErrorNotification.class));
-    }
-}
+//package uk.ac.soton.comp2211;
+//
+//import javafx.application.Platform;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import uk.ac.soton.comp2211.Utility.ErrorNotificationService;
+//import uk.ac.soton.comp2211.control.TopDownViewController;
+//import uk.ac.soton.comp2211.dataStructure.ErrorNotification;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.verify;
+//
+//public class MessageTest {
+//    private ErrorNotificationService service;
+//    private TopDownViewController controller;
+//
+//    @BeforeEach
+//    void setUp() {
+//        controller = Mockito.mock(TopDownViewController.class);
+//        service = new ErrorNotificationService(controller);
+//    }
+//
+//    @BeforeAll
+//    public static void setUpClass() {
+//        Platform.startup(() -> {});
+//    }
+//
+//    @Test
+//    void testNotifyError() throws InterruptedException {
+//        service.notifyError("2024-10-10 12:00", "Test Error Message");
+//        Thread.sleep(100);
+//        verify(controller).addErrorNotification(any(ErrorNotification.class));
+//    }
+//}
